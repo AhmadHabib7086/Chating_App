a806c59baa688e589a7473595414f429efb49b17
Ahmad Habib <ahmadhabib70867086@gmail.com>
2026-03-09 17:37:13 -0700

    fixed error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,16 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+//   withCredentials: true,
+// });
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : "https://chating-app-8.onrender.com/api",
   withCredentials: true,
 });
